f6301c71bcfe15ee65a352c7125a35c49c596c23
SaintGorayeb <SaintGorayeb@users.noreply.github.com>
2020-03-27 13:36:20 -0300

    first commit

---
+++ web.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def somar(a, b):
+    return a + b
+
+@app.route('/ValbertoEnxamista')
+
+def home():
+    soma = somar(5,2)
+    return 'Hello World'
+
+app.run(debug = True)
+
